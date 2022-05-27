73e204f8727122c7d029ac57a84f6dd8cb381d7c
dritter44 <dreither44@gmail.com>
2022-05-27 08:02:21 -0500

    anagrams w/ sort

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,7 @@
-# Don't forget to run your tests!
-
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+    str1_arr = sorted(list(string1.lower()))
+    str2_arr = sorted(list(string2.lower()))
+    for i,x in enumerate(str1_arr) :
+        if str1_arr[i] != str2_arr[i]:
+            return False
+    return True
